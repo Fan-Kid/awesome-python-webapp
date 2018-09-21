a998cc54595d140b4a5daf691e30cff31ab7ca1d
Ice <302890512@qq.com>
2018-09-21 12:16:51 +0800

    构建前端

--- www/handlers.py
+++ www/handlers.py
@@ -8,10 +8,22 @@ from coroweb import get, post
 
 from models import User, Comment, Blog, next_id
 
+#@get('/')
+# async def index(request):
+#     users = await User.findAll()
+#     return {
+#         '__template__' : 'test.html',
+#         'users': users
+#     }
 @get('/')
-async def index(request):
-    users = await User.findAll()
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs=[
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something new', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name="Learn Switft", summary=summary, created_at=time.time()-100000)
+    ]
     return {
-        '__template__' : 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs':blogs
     }
